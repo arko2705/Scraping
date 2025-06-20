ed9821fa07c43c7e4a7df35d3fe73e2a61433fee
arko2705 <arkadeep.mitmpl2023@learner.manipal.edu>
2025-06-20 17:57:10 +0530

    Main code updated,copy kept

--- main.py
+++ main.py
@@ -4,22 +4,15 @@ import pyautogui # type: ignore
 import undetected_chromedriver as udc # type: ignore
 import time
 from seleniumbase import Driver
-from selenium.webdriver.support.ui import WebDriverWait # type: ignore
-from selenium.webdriver.support import expected_conditions as EC # type: ignore
-import os
 import csv
-import sys
 import search as s
-#didnt use beautiful soup,i used selenium cuz google maps is javascript rendered,beautiful soup and requests would haave given back a bs page
+import sys
 def main():
     driver=udc.Chrome(use_subprocess=False,)
     googling_it=s.search()
     driver.get(googling_it)
     print("Enter the number of companies you would like to get the information for: ")
     loop_number=input()
-    #coordinate=driver.find_element(By.CSS_SELECTOR,".fontTitleLarge.IFMGgb")
-    #coordinate.click()
-    #<generating all the profile links>
     cond=True
     while cond:
         try:
@@ -35,23 +28,17 @@ def main():
         link_list.append(i.get_attribute("href"))
     driver.quit()
     start=time.time()
-    #</generating all the profile links>
-    #<Getting the info from the links now>
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("prefs", { "profile.managed_default_content_settings.images": 2,})
-    options.add_argument("--headless=new")
-    driven=webdriver.Chrome(service=service,options=options)
+    driven=Driver(uc=True, headless=True)
     driven.implicitly_wait(5)
     loop_count=0
     element_list=[]
     for i in link_list:#Mostly fixed
      if loop_count<int(loop_number):
-       driven.get(i)
+       driven.uc_open_with_reconnect(i, reconnect_time=0.1)
        time.sleep(0.3)
        try: 
         CFinder=driven.find_element(By.CSS_SELECTOR, ".m6QErb.Pf6ghf.XiKgde.ecceSd.tLjsW")
-        #time.sleep(0.1)
-        time.sleep(0.5)
+        time.sleep(0.1)
         NeedToStrip=CFinder.get_attribute("aria-label")
         Company=NeedToStrip.replace("Actions for ","")
        except:
@@ -60,18 +47,15 @@ def main():
 
        try:   
          AFinder=driven.find_element(By.CLASS_NAME,"CsEnBe")
-         #time.sleep(0.1)
-         time.sleep(0.2)
+         time.sleep(0.1)
          NeedToClean=AFinder.get_attribute("aria-label")
          Address=NeedToClean.replace("Address: ","")
        except:
          print("Trouble rendering address")
          Address="Not present on google business profiles"
        PFinders=driven.find_elements(By.CSS_SELECTOR,".Io6YTe.fontBodyMedium.kR99db.fdkmkc")
-       time.sleep(0.09)
        for f in PFinders:
          try: 
-          #a=i.text.replace(" ","")
              b=int(f.text.replace(" ",""))+1 
              PhoneNumber=f.text.replace(" ","")
              break#will have to change.Numbers cant have spaces.Hence number strings with spaces cant be converted to int 
@@ -82,7 +66,7 @@ def main():
           ProbableWebSites=[]
           for g in WebFinder:
              ProbableWebSites.append(g.get_attribute("href"))
-          time.sleep(0.2)
+         
           for h in ProbableWebSites:
              if h.startswith("http") and not h[-1].isdigit() and not h.startswith("https://api.whatsapp"):
                    WebSite=h
@@ -97,26 +81,21 @@ def main():
           for f in Company_array:
              initial_query=initial_query+"+"+f
           final_query=initial_query+"+email+address"
-          driver=Driver(uc=True, headless=True)
-          driver.uc_open_with_reconnect(final_query, reconnect_time=6)
-          driver.find_element(By.XPATH,"//div[@class='sjVJQd pt054b']").click()
+          driven.uc_open_with_reconnect(final_query, reconnect_time=0.1)
+          driven.find_element(By.XPATH,"//div[@class='sjVJQd pt054b']").click()
        except:
           pass
        try:
-          emailwebelements = driver.find_elements(By.XPATH, "//em[contains(text(),'@')]") 
+          emailwebelements = driven.find_elements(By.XPATH, "//em[contains(text(),'@')]") 
+          emaillist=[]
+          for b in emailwebelements:
+              emaillist.append(b.text)
        except:
+          driven.save_screenshot("debug.png")
           emailwebelements="Not found"
-       emaillist=[]
-       for b in emailwebelements:
-          emaillist.append(b.text)
-          
-       condition=True
-       for x in element_list:
-         if Address in x:
-           condition=False
-       if condition:
-            element_list.append([Company,Address,PhoneNumber,i,WebSite,emaillist])
-            loop_count=loop_count+1
+       
+       element_list.append([Company,Address,PhoneNumber,i,WebSite,emaillist])
+       loop_count=loop_count+1
      else:
        break
        
@@ -128,12 +107,11 @@ def main():
 
     store_in_csv=input("Store in csv:Yes or no:\n")
     if store_in_csv.lower()=="yes":
-      with open(f'Business Profile-{s.your_query}.csv','w',newline='',encoding='UTF-8') as file:  #your_query has to be global to be accessed
+      with open(f'iBusiness Profile-{s.your_query}.csv','w',newline='',encoding='UTF-8') as file:  #your_query has to be global to be accessed
         Columns=['Company Name','Address','Phone Number','Google business profile link','Website','E-Mail']
         write=csv.writer(file)
         write.writerow(Columns)
         write.writerows(element_list)
-        #3os.startfile(f'Google business profile-{your_query}.csv') 
         print("Remember to extend your row width in the csv files,for it to look prettier and better!")       
 
     driven.quit()
